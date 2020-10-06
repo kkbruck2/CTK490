244085836bccc94ca2a8b7b65aa940fe2418d28b
Kim Brucker <kkbruck2@ilstu.edu>
2020-10-06 15:30:45 -0500

    updated limb NAMES

--- p5/cat/sketch.js
+++ p5/cat/sketch.js
@@ -1,11 +1,11 @@
 var catWhole;
 var catHead;
-var legleftF;
-var legrightF;
+var frontL;
+var frontR;
 var cattail;
 var catbody;
-var pawleftB;
-var pawrightB;
+var backL;
+var backR;
 var woodFloor;
 var activeArea;
 var stomachX = 64;
@@ -17,12 +17,12 @@ var marks
 
 function preload() {
   catHead = loadImage("assets/1x/head.png");
-  legleftF = loadImage("assets/1x/frontL.png");
-  legrightF = loadImage("assets/1x/frontR.png");
+  frontL = loadImage("assets/1x/frontL.png");
+  frontR = loadImage("assets/1x/frontR.png");
   cattail = loadImage("assets/1x/tail.png");
   catbody = loadImage("assets/1x/body.png");
-  pawleftB = loadImage("assets/1x/backL.png");
-  pawrightB = loadImage("assets/1x/backR.png");
+  backL = loadImage("assets/1x/backL.png");
+  backR = loadImage("assets/1x/backR.png");
   activeArea = loadImage("assets/1x/Asset106.png");
   catWhole = loadImage("assets/1x/catPlace.png");
   marks = loadImage("assets/1x/marking.png");
@@ -68,13 +68,13 @@ function cat() {
   rotate(angle++);
   translate(20, 30);
   pop();
-  image(legleftF, 62, 54);
+  image(frontL, 62, 54);
   //right front Leg
-  image(legrightF, 131, 63);
+  image(frontR, 131, 63);
   //back Legs left
-  image(pawleftB, 62, 176);
+  image(backL, 62, 176);
   //back right
-  image(pawrightB, 134, 177);
+  image(backR, 134, 177);
 
 
   //Cat body gets fat
