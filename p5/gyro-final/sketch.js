0eb1c428a2e3feb00af86a84ddfe7b26e6ab232f
Kim Brucker <kkbruck2@ilstu.edu>
2020-11-22 16:02:04 -0600

    mice working can't find cat

--- p5/gyro-final/sketch.js
+++ p5/gyro-final/sketch.js
@@ -3,19 +3,40 @@
 //(Rules: Cohesion, Separation, Alignment.)<br>
 //From <a href='http://natureofcode.com'>natureofcode.com</a>
 //===============variables
-//var mice = [];
-//var mices = [];
+
+//===============mice
 let boids = [];
 var flock;
 var woodFloor;
 var mice;
 
+var catHead;
+var frontL;
+var frontR;
+var cattail;
+var catbody;
+var backL;
+var backR;
+var marks;
+var stomachY = 64;
+var stomachX = 160;
+var catDirection = 1;
+//==============environment
+var woodFloor;
+var windowWidth = 0;
+var windowHeight = 0;
+
+//===============axis
+let x = 150,
+  y = 150,
+  angle1 = 0.0,
+  segLength = 100;
+
+
 
 function preload() {
+  //================mice
   mice = loadImage('assets/1x/mice5.png');
-  woodFloor = loadImage('assets/1x/woodbkg.jpg');
-
-
   // mice[0] = loadImage('assets/1x/mice1.png');
   // mice[1] = loadImage('assets/1x/mice2.png');
   // mice[2] = loadImage('assets/1x/mice3.png');
@@ -24,26 +45,91 @@ function preload() {
   // mice[5] = loadImage('assets/1x/mice4.png');
   // mice[6] = loadImage('assets/1x/mice3.png');
   // mice[7] = loadImage('assets/1x/mice2.png');
+
+
+  //===============cat
+  catHead = loadImage("assets/1x/head.png");
+  frontL = loadImage("assets/1x/frontL.png");
+  frontR = loadImage("assets/1x/frontR.png");
+  cattail = loadImage("assets/1x/tail.png");
+  backL = loadImage("assets/1x/backL.png");
+  backR = loadImage("assets/1x/backR.png");
+  //catWhole = loadImage("assets/1x/catPlace0.png");
+  woodFloor = loadImage("assets/1x/myFloor.png");
+  marks = loadImage("assets/1x/marking.png");
+
 }
 //===============setup
 function setup() {
-  createCanvas(1080, 720);
+  //createCanvas(1080, 720);
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+
 
   flock = new Flock();
   // Add an initial set of boids into the system
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 50; i++) {
     let b = new Boid(random(width), random(height));
     flock.addBoid(b);
   }
   imageMode(CENTER);
+
+  catPos = createVector(windowWidth / 2, windowHeight / 2);
+
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
+
 }
 //==================draw
 function draw() {
+
+
   background(200, 150, 100);
   image(woodFloor, width/2, height/ 2, width, height);
     flock.run();
+    this.Cat();
+
+  // the map command !!!!
+  // takes your variable and maps it from range 1 to range 2
+  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
+  xPosition = map(gamma, -60, 60, 0, width);
+  yPosition = map(beta, -30, 30, 0, height);
+
+
+  catPos.x = xPosition
+  catPos.y = yPosition
+
+  for (var i = 0; i < boids.length; i++) {
+    boids[i].display();
+    boids[i].drive();
+    if (boids[i].pos.dist(catPos) < 50) {
+      boids.splice(i, 1);
+      stomachY += 3;
+
+
+    }
+  }
+
 
 }
+//------------------------------------------------- Read in accelerometer data
+window.addEventListener('deviceorientation', function(e) {
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+});
+
+// ------------------------------------------------------accelerometer Data
+window.addEventListener('devicemotion', function(e) {
+  // get accelerometer values
+  x = e.acceleration.x;
+  y = e.acceleration.y;
+  z = e.acceleration.z;
+});
+//----------------------------------------------------------element definitions
+
+
 
 
 // The Nature of Code
@@ -55,7 +141,6 @@ function draw() {
 
 function Flock() {
   // An array for all the boids
-
   this.boids = [];
   // Initialize the array
 }
@@ -99,23 +184,6 @@ Path.prototype.run = function(boids) {
   }
 }
 
-
-// distance = p5.dist(predictLocation, normalPoint);
-// if (distance < path.radius) {
-//   seek(normalPoint);
-// }
-// distance = p5.dist(predictLocation, normalPoint);
-// if (distance > path.radius) {
-//   b.normalize();
-//   b.mult(50);
-// }
-// target = p5.Vector.add(normalPoint, b);
-// seek(target);
-
-
-
-
-
 // The Nature of Code
 // Daniel Shiffman
 // http://natureofcode.com
@@ -129,7 +197,7 @@ function Boid(x, y) {
   this.position = createVector(x, y);
   this.r = 3.0;
   this.maxspeed = 5.0; // Maximum speed
-  this.maxforce = .5 // Maximum steering force
+  this.maxforce = 0.5 // Maximum steering force
   //   this.maxforce = 0.05; // Maximum steering force
 }
 
@@ -138,6 +206,7 @@ Boid.prototype.run = function(boids) {
   this.update();
   this.borders(100, 100);
   this.render();
+
 }
 
 Boid.prototype.applyForce = function(force) {
@@ -296,5 +365,55 @@ Boid.prototype.cohesion = function(boids) {
     return this.seek(sum); // Steer towards the location
   } else {
     return createVector(0, 0);
+  }
+}
+
+function Cat() {
+  dx = catPos.x - x;
+  dy = catPos.y - y;
+  angle1 = atan2(dy, dx);
+  x = catPos.x - cos(angle1) * segLength;
+  y = catPos.y - sin(angle1) * segLength;
+
+ // this.cat = function(cat) {
+ //    cat(catPos.x, catPos.y);
+ // }
+
+  this.drive = function(cat) {
+
+    push();
+    translate(x, y);
+    rotate(a);
+    this.Cat(windowWidth / 2, windowHeight / 2);
+    //line(0, 0, segLength, 0);
+    pop();
+
+  }
+
+  this.display = function(cat) {
+    push();
+    //==frontpaws
+    image(frontL, 63, -30 + -2 / 10 * (stomachY - 30))
+
+    image(frontR, 63, 35 + 2 / 10 * (stomachY - 35));
+
+    //back Legs left
+    image(backL, -63, -20 + -2 / 7 * (stomachY - 30));
+    //back right
+    image(backR, -63, 18 + 2 / 7 * (stomachY - 25));
+
+    //Cat body gets fat
+    fill(115, 99, 87);
+    stroke(0);
+    strokeWeight(2);
+    ellipse(-30, 0, 165, stomachY);
+    //body markings
+    noStroke();
+    image(marks, -30, 0, 160, stomachY);
+    //head and tail
+    image(catHead, 80, 0);
+    image(cattail, -160, 20);
+
+    pop();
   }
 }
