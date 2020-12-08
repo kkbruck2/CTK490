97f7a21d90f638d5bc1e2b6875e611e26a49bda9
Kim Brucker <kkbruck2@ilstu.edu>
2020-12-07 18:22:03 -0600

    game update

--- p5/gyro-final-vlock1/sketch.js
+++ p5/gyro-final-vlock1/sketch.js
@@ -6,12 +6,12 @@
 
 //===============mice
 let boids = [];
-var flock;
-var woodFloor;
+let flock;
+//--------------------mice array
 var mice = [];
-let numFrame = 8;
+let miceNum = 8;
 let currentFrame = 0;
-
+//===============cat
 var catHead;
 var frontL;
 var frontR;
@@ -22,16 +22,15 @@ var backR;
 var marks;
 var stomachY = 159;
 var stomachX = 64;
-
 var catPos;
-var catPos0;
 var cat;
 var xdirection = 1;
 var ydirection = 1;
+var windowWidth = 0;
+var windowHeight = 0;
 //==============environment
 var woodFloor;
-var displayWidth = 0;
-var displayHeight = 0;
+
 
 //===============axis
 let base = 150,
@@ -46,12 +45,25 @@ var zPosition = 0;
 var x = 0; // acceleratiobn data
 var y = 0;
 var z = 0;
-var windowWidth = 0;
-var windowHeight = 0;
+//==================interface var
+var start;
+var win;
+var lose;
+var winSound;
+var loseSound;
+var soundOn;
+var soundOff;
+
+//------------------game
+
+
+var myState = 0;
+var timer = 0;
+var direction = [];
 
 
 function preload() {
-  //================mice
+  //================mice images
   //mice = loadImage('assets/1x/mice5.png');
   mice[0] = loadImage('assets/1x/mice1.png');
   mice[1] = loadImage('assets/1x/mice2.png');
@@ -62,24 +74,43 @@ function preload() {
   mice[6] = loadImage('assets/1x/mice3.png');
   mice[7] = loadImage('assets/1x/mice2.png');
 
-  //===============cat
+  //===============cat images
   catHead = loadImage("assets/1x/head.png");
   frontL = loadImage("assets/1x/frontL.png");
   frontR = loadImage("assets/1x/frontR.png");
   cattail = loadImage("assets/1x/tail.png");
   backL = loadImage("assets/1x/backL.png");
   backR = loadImage("assets/1x/backR.png");
-  //catWhole = loadImage("assets/1x/catPlace0.png");
   woodFloor = loadImage("assets/1x/myFloor.png");
   marks = loadImage("assets/1x/marking.png");
+  //=================interface images
+  start = loadImage('assets/1x/start.png');
+  win = loadImage('assets/1x/winCat.png');
+  lose = loadImage('assets/1x/loss.png');
+  soundOn = loadImage('assets/1x/soundOn.png')
+  soundOff = loadImage('assets/1x/soundOff.png')
+  //===============sound
+  bkgMusic = loadSound('assets/456797__anechoix__jazz-music-loop.mp3');
+  winSound = loadSound('assets/396174__funwithsound__success-triumph-resolution-sound-effect_01.mp3')
+  loseSound = loadSound('assets/174427__badly99__domino-sound-effect_01.mp3')
 
+  bkgMusic.loop();
+  bkgMusic.stop();
+  winSound.play();
+  winSound.stop();
+  loseSound.play();
+  loseSound.stop();
 }
 //===============setup
 function setup() {
-  //createCanvas(1080, 720);
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(768, 1366);
   angleMode(DEGREES);
   imageMode(CENTER);
+  ellipseMode(CENTER);
+
+  //---------------fonts
+  fontDiner = loadFont('assets/FontdinerSwanky-Regular.ttf');
+  robo = loadFont('assets/Roboto-Regular.ttf');
 
   flock = new Flock();
   // Add an initial set of boids into the system
@@ -88,7 +119,7 @@ function setup() {
     flock.addBoid(b);
   }
 
-  catPos = createVector(0, 0);
+  catPos = createVector(windowWidth / 2, windowHeight / 2);
 
   alpha = 0;
   beta = 0;
@@ -100,53 +131,108 @@ function setup() {
 //==================draw
 function draw() {
   background(200, 150, 100);
-  image(woodFloor, width / 2, height / 2);
-
-
-  let catPos0 = createVector(windowWidth / 2, windowHeight / 2);
-  let catPos = createVector(xPosition - windowWidth / 2, yPosition - windowHeight / 2);
-  drawAxis(catPos0, catPos);
-
-  let myHeading = catPos.heading();
-
-  flock.run();
-
-  // if (catPos.x > windowWidth || catPos.x < windowWidth) {
-  //   xdirection *= -1;
-  // }
-  // if (catPos.y > windowHeight || catPos.Y < windowHeight) {
-  //   ydirection *= -1;
-  // }
-
-
-  // the map command !!!!
-  // takes your variable and maps it from range 1 to range 2
-  // map(yourVar, range1_x, range1_y, range2_x, range2_y) ;
-  //================switched x and y maps
-  zPosition = map(alpha, 0, 0, 0, 0);
-  xPosition = map(gamma, -60, 60, 0, width);
-  yPosition = map(beta, -30, 30, 0, height);
+  image(woodFloor, windowWidth / 2, windowWidth / 2, windowWidth, windowWidth);
 
 
   catPos.x = xPosition
   catPos.y = yPosition
 
+  zPosition = map(alpha, 0, 0, 0, 0);
+  yPosition = map(gamma, 30, -30, 0, width);
+  xPosition = map(beta, 60, -60, 0, height);
 
 
-  for (var i = 0; i < flock.boids.length; i++) {
-    //flock.boids[i].render();
-    flock.boids[i].run();
-    if (flock.boids[i].position.dist(catPos) < 40) {
-      flock.boids.splice(i, 1);
-      stomachX += 2;
 
-    }
+  textFont(fontDiner);
 
+
+  switch (myState) {
+
+    case 0:
+      fill(0);
+      textSize(70);
+      textAlign(CENTER);
+      text("Let's Hunt Mice!", width / 2, 250);
+      image(start, width / 2, height / 2 + 30);
+      textSize(24);
+      textFont(robo);
+      text("For best results turn on screen lock.", width / 2, height - 80);
+      textFont(fontDiner);
+      textSize(36);
+      text("Click to Enter Game", width / 2, height - 200);
+      break;
+
+    case 1:
+      bkgMusic.play();
+      myState = 2;
+      break;
+
+    case 2:
+      fill(0);
+      textSize(70);
+      textAlign(CENTER);
+      text("Let's Hunt Mice!", width / 2, 250);
+      image(start, width / 2, height / 2 + 30);
+      textSize(24);
+      textFont(robo);
+      textFont(fontDiner);
+      textSize(36);
+
+      text("Click to Start Game", width / 2, height - 200);
+      break;
+
+
+    case 3:
+      game();
+      timer++;
+      if (timer > 120) {
+        myState = 6;
+        timer = 0;
+      }
+      break;
+
+    case 4:
+      winSound.play();
+      bkgMusic.stop();
+      myState = 5;
+      break;
+
+    case 5: // the win state
+      fill(0);
+      textFont(fontDiner);
+      textSize(60);
+      textAlign(CENTER);
+      text("Hee...Hee! WE WON!", width / 2 - 15, 120);
+      image(win, width / 2, height / 2);
+      textSize(24);
+      textFont(robo);
+      text("Click to Reset Game", width / 2, height - 35);
+
+      break;
+
+    case 6:
+      bkgMusic.stop();
+      loseSound.play();
+      myState = 6;
+      break;
+
+    case 7: // the lose state
+      fill(0);
+      textFont(fontDiner);
+      textSize(60);
+      textAlign(CENTER);
+      text("uh...oh!", width / 2, 120);
+      image(lose, width / 2, 400);
+      textSize(24);
+      textFont(montMed);
+      text("Click to Reset Game", width / 2, height - 35);
+
+
+      break;
   }
-
 }
-//================end draw
 
+//================end draw
 //---------------- Read in accelerometer data
 window.addEventListener('deviceorientation', function(e) {
   alpha = e.alpha;
@@ -161,10 +247,8 @@ window.addEventListener('devicemotion', function(e) {
   y = e.acceleration.y;
   z = e.acceleration.z;
 });
-//------------------element definitions
-// Flock object
-// Does very little, simply manages the array of all the boids
 
+//=====================Flock
 
 function Flock() {
   // An array for all the boids
@@ -172,15 +256,19 @@ function Flock() {
   // Initialize the array
 }
 
-Flock.prototype.run = function() {
+//   Flock.prototype.run = function() {
 
-}
+//   }
 
 Flock.prototype.addBoid = function(b) {
   flock.boids.push(b);
 }
 
-// Path class
+//------------------element definitions
+// Flock object
+// Does very little, simply manages the array of all the boids
+
+//===================== Path class
 function Path(p) {
   let radius = 25;
 
@@ -215,15 +303,17 @@ Path.prototype.run = function(boids) {
 // http://natureofcode.com
 
 // Boid class
-// Methods for Separation, Cohesion, Alignment added
+// Methods for Separation, Cohesion,
 //===================== Boid start
 function Boid(x, y) {
   this.acceleration = createVector(0, 0);
-  this.velocity = createVector(random(-20, 20), random(-20, 20));
+  this.velocity = createVector(random(-10, 10), random(-10, 10));
   this.position = createVector(x, y);
   this.r = 3.0;
-  this.maxspeed = 20; // Maximum speed
+  this.maxspeed = 10; // Maximum speed
   this.maxforce = 0.5 // Maximum steering force
+  this.timer = 0;
+  this.maxTimer = (1, 10);
   //   this.maxforce = 0.05; // Maximum steering force
 }
 //---------------------end boid variables
@@ -231,9 +321,12 @@ Boid.prototype.run = function() {
   //==================these are the prototypes for boid
   this.flock(boids);
   this.update();
-  this.borders();
+  this.borders(100, 100);
   this.render();
+
 }
+
+
 //---------------------- end run
 Boid.prototype.applyForce = function(force) {
   // We could add mass here if we want A = F / M
@@ -283,7 +376,6 @@ Boid.prototype.seek = function(target) {
 //------------------ end seek
 
 Boid.prototype.render = function() {
-  // Draw a triangle rotated in the direction of velocity
   let theta = this.velocity.heading(0, 0);
   fill(127);
   stroke(200);
@@ -293,26 +385,60 @@ Boid.prototype.render = function() {
 
   image(mice[currentFrame], 0, 0);
   currentFrame++;
-  if (currentFrame == mice.length) {
+  if (miceNum == mice.length) {
+    currentFrame = 0;
+  }
+  pop();
+}
+
+
+//==================Mice movement
+Boid.prototype.movement = function() {
+  //  translate(p5.Vector.fromAngle(millis() / 1000, 40));
+
+  push();
+  // animating the mices
+  if (this.vel > 0) map(this.maxTimer * -1 === this.vel.mag());
+  if (this.vel < 0) map(this.maxTimer === this.vel.mag());
+  translate(this.pos.x, this.pos.y);
+  rotate(this.vel.heading());
+  image(mice[this.miceNum], 0, 0);
+  this.timer++;
+
+
+  if (this.timer > this.maxTimer) {
+    this.miceNum = this.miceNum + 1;
+    this.timer = 0;
+
+  }
+
+
+  //mice reset
+  if (miceNum > mice.length - 1) {
     currentFrame = 0;
   }
 
+
   pop();
+
 }
+
+
 // --------------------- end render
 // Wraparound
 Boid.prototype.borders = function() {
-  if (this.position.x < 0) this.position.x = windowWidth;
-  if (this.position.y < 0) this.position.y = windowHeight;
-  if (this.position.x > windowWidth) this.position.x = 0;
-  if (this.position.y > windowHeight) this.position.y = 0;
+  if (this.position.x < -150) this.position.x = windowWidth + 150;
+  if (this.position.y < -150) this.position.y = windowHeight + 150;
+  if (this.position.x > windowWidth + 150) this.position.x = -150;
+  if (this.position.y > windowHeight + 150) this.position.y = -150;
 }
 //--------------------- end borders
 // Separation
 // Method checks for nearby boids and steers away
+
 Boid.prototype.separate = function(boids) {
   let angle = 0;
-  let angleVel = 0.3;
+  let angleVel = 0.5;
   let desiredseparation = 160;
   let steer = createVector(0, 0);
   let count = 0;
@@ -346,10 +472,10 @@ Boid.prototype.separate = function(boids) {
 }
 //--------------------end Separation
 
-// Cohesion
+//=====================Cohesion
 // For the average location (i.e. center) of all nearby boids, calculate steering vector towards that location
 Boid.prototype.cohesion = function(boids) {
-  let neighbordist = 100;
+  let neighbordist = 50;
   let sum = createVector(0, 0); // Start with empty vector to accumulate all locations
   let count = 0;
   for (let i = 0; i < flock.boids.length; i++) {
@@ -367,6 +493,7 @@ Boid.prototype.cohesion = function(boids) {
   }
 }
 //-------------------- end cohesion
+
 //============================================end boid class
 
 //===================================start Cat class
@@ -375,7 +502,7 @@ function drawAxis(base, vec) {
   push();
   noStroke();
   translate(base.x, base.y);
-  line(base.x, base.y, vec.x, vec.y);
+  line(0, 0, vec.x, vec.y);
   rotate(vec.heading());
   translate(vec.mag(), 0);
   cat(vec.x, vec.y);
@@ -412,8 +539,68 @@ function cat() {
 
   image(catHead, 2, 2);
   image(cattail, 20, 261);
-  //fill(150, 0, 150, 150);
   translate(-300, -300);
-  //ellipse(300, 300, 70, 70);
   pop();
+}
+//------------------------end cat class
+//================mouseReleased
+function mouseReleased() {
+  switch (myState) {
+    case 0:
+      myState = 1;
+      break;
+
+    case 2:
+      myState = 3;
+      break;
+    case 6:
+      resetTheGame();
+      myState = 1;
+      break;
+    case 7:
+      resetTheGame();
+      myState = 0;
+      break;
+  }
+}
+//================end mouseReleased
+//------------------------------------- game
+function game() {
+
+  let catPos0 = createVector(windowWidth / 2, windowHeight / 2);
+  let catPos = createVector(xPosition - windowWidth / 2, yPosition - windowHeight / 2);
+  drawAxis(catPos0, catPos);
+
+  let myHeading = catPos.heading();
+
+
+
+  //-----------------splice mice
+  for (var i = 0; i < flock.boids.length; i++) {
+    flock.boids[i].run();
+    if (flock.boids[i].position.dist(catPos) < 50) {
+      flock.boids.splice(i, 1);
+      stomachX += 2;
+    }
+
+    if (flock.boids.length == 0) {
+      myState = 3;
+      timer = 0;
+    }
+
+
+
+  }
+}
+//------------------ game end
+//==================reset game
+function resetTheGame() {
+boids = [];
+
+  for (let i = 0; i < 30; i++) {
+    let b = new Boid(random(width), random(height));
+    flock.addBoid(b);
+    timer = 0;
+    stomachX = 64;
+  }
 }
